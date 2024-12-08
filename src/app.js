9086b1cb8e164bd8a624144c729eb920eafc6369
Aguthankgod <Aguthankgod@gmail.com>
2024-12-08 17:27:12 +0100

    -add a route for auth, -add a user profile route update, and a general middleware for the route.  this middleware checks for invalidated tokens and also for route security
     -implemented a check for the presence of environmental variables. this checks terminates execution flow if not found.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
 require("./config/db")();
 const websocket = require("ws");
+const { createServer } = require("http");
+
+const Error_mw = require("./middleware/error_mw");
+const verifyToken_mw = require("./middleware/verifyToken_mw");
+const authorisation_mw = require("./middleware/authorisation_mw");
+
+if (!process.env.jwtsecret) {
+  throw new Error("FATAL ERROR: jwtsecret is not defined.");
+}
+
 const app = express();
 
-const { createServer } = require("http");
+app.use(Error_mw);
+app.use(express.json());
+app.use("/api/auth", require("./routes/auth_route"));
+app.use("/api/users", verifyToken_mw, authorisation_mw, require("./routes/user_route"));
+app.use("");
+
 const server = createServer(app);
 const wss = new websocket.Server({ server });
+
+wss.on("connection", () => {});
 
 module.exports = app;
