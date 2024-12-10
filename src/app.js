51f2fd4cb4ac5ee5091c7f17119c2b64618384ef
Aguthankgod <Aguthankgod@gmail.com>
2024-12-10 04:40:30 +0100

    import the created websocket server

--- src/app.js
+++ src/app.js
@@ -1,28 +1,33 @@
+// @ts-nocheck
 require("dotenv").config();
 const express = require("express");
 require("./config/db")();
-const websocket = require("ws");
+const WebSocket = require("ws");
 const { createServer } = require("http");
+const jwt = require("jsonwebtoken");
+const logger = require("../logger");
 
 const Error_mw = require("./middleware/error_mw");
 const verifyToken_mw = require("./middleware/verifyToken_mw");
 const authorisation_mw = require("./middleware/authorisation_mw");
+const webSocket = require("./util/webSocket");
 
 if (!process.env.jwtsecret) {
   throw new Error("FATAL ERROR: jwtsecret is not defined.");
 }
 
-const app = express();
-
-app.use(Error_mw);
 app.use(express.json());
+
+// Routes
 app.use("/api/auth", require("./routes/auth_route"));
 app.use("/api/users", verifyToken_mw, authorisation_mw, require("./routes/user_route"));
-app.use("");
+app.use(Error_mw);
+const app = express();
 
+// HTTP & WebSocket Server
 const server = createServer(app);
-const wss = new websocket.Server({ server });
+const wss = new WebSocket.Server({ server });
 
-wss.on("connection", () => {});
+webSocket(wss);
 
 module.exports = app;
