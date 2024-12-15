8d0eca8fed2d341f0334454c563552aa25cb9147
Aguthankgod <Aguthankgod@gmail.com>
2024-12-16 00:45:59 +0100

    added extra route for  friends activities,conversations and messages

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const logger = require("../logger");
 const Error_mw = require("./middleware/error_mw");
 const verifyToken_mw = require("./middleware/verifyToken_mw");
 const authorisation_mw = require("./middleware/authorisation_mw");
-const webSocket = require("./util/webSocket");
+const { webSocket } = require("./util/webSocket");
 
 if (!process.env.jwtsecret) {
   throw new Error("FATAL ERROR: jwtsecret is not defined.");
@@ -21,6 +21,15 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", require("./routes/auth_route"));
 app.use("/api/users", verifyToken_mw, authorisation_mw, require("./routes/user_route"));
+app.use("/api/friend/", verifyToken, authorisation_mw, require("./routes/friend_route"));
+app.use("/api/messages", verifyToken_mw, authorisation_mw, require("../src/routes/message_route"));
+app.use(
+  "/api/conversations",
+  verifyToken_mw,
+  authorisation_mw,
+  required("../src/routes/conversation_route.js")
+);
+
 app.use(Error_mw);
 const app = express();
 
