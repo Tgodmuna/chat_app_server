62380d50cb46919a45c342b2c7575fadf53425a4
Aguthankgod <Aguthankgod@gmail.com>
2024-12-18 02:49:01 +0100

    fix: remove former implementation which overrides the document with the new changes without modifying the requested field

--- src/controllers/setMessageStatus.js
+++ src/controllers/setMessageStatus.js
@@ -1,6 +1,10 @@
 const MESSAGE = require("../models/message_model");
-async function SetMessageStatus(messageID, status) {
-  return await MESSAGE.findByIdAndUpdate({ _id: messageID }, { read: status }, { new: true });
+async function SetMessageStatus(messageID, user_id, conversationID) {
+  return await MESSAGE.findByIdAndUpdate(
+    { _id: messageID, conversationID, receiver: user_id, read: false },
+    { $set: { read: true } },
+    { new: true }
+  );
 }
 
 module.exports = SetMessageStatus;
