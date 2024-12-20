1cbdf3a871759eae163946c49cd72a47d2516c1a
Aguthankgod <Aguthankgod@gmail.com>
2024-12-20 03:43:59 +0100

    feat: refactor WebSocket server initialization and correct middleware usage in app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ const logger = require("../logger");
 const Error_mw = require("./middleware/error_mw");
 const verifyToken_mw = require("./middleware/verifyToken_mw");
 const authorisation_mw = require("./middleware/authorisation_mw");
-const { webSocket } = require("./util/webSocket");
+const { WebSocketServer } = require("./util/webSocket");
+const app = express();
 
 if (!process.env.jwtsecret) {
   throw new Error("FATAL ERROR: jwtsecret is not defined.");
@@ -21,22 +22,21 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", require("./routes/auth_route"));
 app.use("/api/users", verifyToken_mw, authorisation_mw, require("./routes/user_route"));
-app.use("/api/friend/", verifyToken, authorisation_mw, require("./routes/friend_route"));
+app.use("/api/friend", verifyToken_mw, authorisation_mw, require("./routes/friend_route"));
 app.use("/api/messages", verifyToken_mw, authorisation_mw, require("../src/routes/message_route"));
 app.use(
   "/api/conversations",
   verifyToken_mw,
   authorisation_mw,
-  required("../src/routes/conversation_route.js")
+  require("../src/routes/conversation_route.js")
 );
 
 app.use(Error_mw);
-const app = express();
 
 // HTTP & WebSocket Server
 const server = createServer(app);
 const wss = new WebSocket.Server({ server });
 
-webSocket(wss);
+WebSocketServer(wss);
 
 module.exports = app;
