fba1e896e83f28d5927286326171c4ca6bd1c921
Aguthankgod <Aguthankgod@gmail.com>
2024-12-21 02:00:34 +0100

    fix: improve user retrieval middleware by correcting parameter access and adding validation

--- src/middleware/getUser_mw.js
+++ src/middleware/getUser_mw.js
@@ -1,10 +1,14 @@
 const logger = require("../../logger");
 const USER = require("../models/user_model");
 module.exports = async function getUser_mw(req, res, next) {
-  const id = req.param.id;
+  const id = req.params.id;
+
+  console.log("idParam:", id);
 
   try {
-    const user = await USER.findById({ id }).select("name age profilePicture email bio").sort("1");
+    if (!id) return res.status(400).send("no param");
+
+    const user = await USER.findById(id).select("-password ").sort("1");
 
     if (!user) return res.status(400).send("no such a user with the ID");
 
