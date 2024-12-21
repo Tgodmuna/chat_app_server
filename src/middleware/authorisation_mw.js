437de1499ad9208bdd8912203dd7431da9479185
Aguthankgod <Aguthankgod@gmail.com>
2024-12-21 01:55:13 +0100

    fix: remove commented-out token blacklist check in authorisation middleware

--- src/middleware/authorisation_mw.js
+++ src/middleware/authorisation_mw.js
@@ -4,9 +4,6 @@ const jwt = require("jsonwebtoken");
 module.exports = function authorised_mw(req, res, next) {
   const token = req.header("x-auth-token");
 
-  //   if (token_Blacklist().has(token))
-  //     return res.status(401).send("not authorised,you are invalidated");
-
   if (!token) return res.status(401).send("access denied.not authorised, no token provided");
 
   if (!jwt.verify(token, process.env.jwtsecret))
