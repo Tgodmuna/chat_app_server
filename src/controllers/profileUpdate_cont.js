cf5b9bdec568561c610a0e44704854812cf1d410
Aguthankgod <Aguthankgod@gmail.com>
2024-12-21 01:54:32 +0100

    fix: add error handling and validation for profile update function

--- src/controllers/profileUpdate_cont.js
+++ src/controllers/profileUpdate_cont.js
@@ -1,23 +1,34 @@
 const USER = require("../models/user_model");
 
 async function profileUpdate_cont(id, body) {
-  const { name, bio, profilePicture, location, interests, status } = body;
+  try {
+    if ((!body && id) || !id || !body) {
+      throw new Error("update value and id is required");
+    }
 
-  //find user by its id and update the requested field
-  return await USER.findByIdAndUpdate(
-    { id },
-    {
-      $set: {
-        name: !name ? null : name,
-        bio: !bio ? null : bio,
-        profilePicture: !profilePicture ? null : profilePicture,
-        location: !location ? null : location,
-        interests: !interests ? null : interests,
-        status: !status ? null : status,
+    const { name, bio, profilePicture, location, interests, status } = body;
+
+    //find user by its id and update the requested field
+    const updated = await USER.findByIdAndUpdate(
+      id,
+      {
+        $set: {
+          name: name && name,
+          bio: bio && bio,
+          profilePicture: profilePicture && profilePicture,
+          location: location && location,
+          interests: interests && interests,
+          status: status && status,
+        },
       },
-    },
-    { new: true }
-  );
+      { new: true }
+    );
+
+    return updated;
+  } catch (err) {
+    console.log(err.message);
+    throw err;
+  }
 }
 
 module.exports = profileUpdate_cont;
