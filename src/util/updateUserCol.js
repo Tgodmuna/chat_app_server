98271628f9a05df95202a626e0d5cfc3074bf457
Aguthankgod <Aguthankgod@gmail.com>
2024-12-27 15:55:35 +0100

    feat: add updateUser_collection function to update user properties in the database

--- src/util/updateUserCol.js
+++ src/util/updateUserCol.js
@@ -1,6 +1,15 @@
 // @ts-nocheck
 const logger = require("../../logger");
 const USER = require("../models/user_model");
+/**
+ * Updates a specified property of a user document in the database.
+ *
+ * @param {string} property - The property of the user document to update.
+ * @param {*} value - The new value to set for the specified property.
+ * @param {string} userID - The ID of the user document to update.
+ * @returns {Promise<Object>} The updated user document.
+ * @throws {Error} If an error occurs during the update process.
+ */
 async function updateUser_collection(property, value, userID) {
   try {
     const user = await USER.findById(userID);
