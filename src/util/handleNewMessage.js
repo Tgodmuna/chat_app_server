6578fd02f22ae4c21885f768f279a696fa132a14
Aguthankgod <Aguthankgod@gmail.com>
2024-12-28 01:52:25 +0100

    feat: enhance handleNewMessage utility to improve message delivery and logging

--- src/util/handleNewMessage.js
+++ src/util/handleNewMessage.js
@@ -1,3 +1,8 @@
+const message_model = require("../models/message_model");
+const deliverMessage = require("./deliverMessage");
+const createConversation = require("./logConversation");
+const saveMessage = require("./logMessage");
+
 /**
  * Handles the process of receiving a new message, including delivering the message in real-time if the recipient is online,
  * creating a conversation log, and saving the message to the database.
@@ -9,15 +14,11 @@
  * @param {Object} ActiveConnections - An object representing the active connections of users.
  * @returns {Promise<void>} A promise that resolves when the message handling process is complete.
  */
-const deliverMessage = require("./deliverMessage");
-const createConversation = require("./logConversation");
-const saveMessage = require("./logMessage");
-
 
 const handleNewMessage = async (userID, recipientID, content, type, ActiveConnections) => {
   // Persist the conversation
   const conversation = await createConversation([userID, recipientID], type);
-  const convoID = conversation._id;
+  const conversation_ID = conversation?._id;
 
   // Deliver real-time message if recipient is online
   if (
@@ -35,17 +36,43 @@ const handleNewMessage = async (userID, recipientID, content, type, ActiveConnec
     deliverMessage(
       userID,
       {
-        event: "messageSent",
-        message: "Message successfully sent!",
+        event: "messageSent&Delivered",
+        message: "Message successfully sent and delivered!",
+        code: 200,
       },
       ActiveConnections
     );
-    await saveMessage(content, userID, recipientID, convoID);
+    await saveMessage(content, userID, recipientID, conversation_ID);
+    return;
   }
 
-  //if message failed to deliver because user was not online.save it to the DB
-  if (!deliverMessage) {
-    await saveMessage(content, userID, recipientID, convoID);
+  //if message failed to deliver because user is not online, save it to the DB
+  const { persistedMessageID } = await saveMessage(content, userID, recipientID, conversation_ID);
+  deliverMessage(
+    userID,
+    { event: "Sent&NotDelivered", message: "sent and not delivered", code: 205 },
+    ActiveConnections
+  );
+
+  //deliver the message if the user comesback online.
+  if (ActiveConnections.has(recipientID)) {
+    const clientSocket = ActiveConnections.get(recipientID);
+
+    //fecth the saved messsage
+    const persistedMessage = await message_model.findById(persistedMessageID);
+    clientSocket.send(JSON.stringify(persistedMessage));
+
+    //notify the sender their message has been delivered
+    deliverMessage(
+      userID,
+      {
+        event: "Delivered",
+        message: `Message delivered to-${userID}`,
+        code: 200,
+      },
+      ActiveConnections
+    );
+
     return;
   }
 };
