41ba491b3af4de9934b651575ac319756682bc1c
Aguthankgod <Aguthankgod@gmail.com>
2024-12-29 16:44:29 +0100

    fix: add validation for message parameters and improve conversation last message update logic

--- src/util/logMessage.js
+++ src/util/logMessage.js
@@ -17,6 +17,18 @@ const CONVERSATION = require("../models/conversation_model");
 const mongoose = require("mongoose");
 
 async function saveMessage(content, sender, receiver, conversationID) {
+  if (!content || !sender || !receiver || !conversationID) {
+    throw new Error("All parameters must be provided and valid.");
+  }
+
+  if (
+    !mongoose.Types.ObjectId.isValid(sender) ||
+    !mongoose.Types.ObjectId.isValid(receiver) ||
+    !mongoose.Types.ObjectId.isValid(conversationID)
+  ) {
+    throw new Error("Invalid ObjectId provided.");
+  }
+
   try {
     const message = new MESSAGE({
       content,
@@ -30,19 +42,22 @@ async function saveMessage(content, sender, receiver, conversationID) {
     await message.save();
 
     //update the conversation last message
-    const conversationLastMessage = CONVERSATION.findByIdAndUpdate(
-      conversationID,
-      { lastMessage: message._id },
-      { new: true }
-    );
+    const conversationLastMessage = await CONVERSATION.findOne({ _id: conversationID });
+    if (conversationLastMessage) {
+      conversationLastMessage.lastMessage = message._id;
+
+      await conversationLastMessage.save();
+    }
 
     if (!conversationLastMessage) throw new Error("conversation last message failed to be updated");
 
-    logger.info("Message saved and lastMessage updated successfully!");
+    process.env.NODE_ENV === "development" &&
+      logger.info("Message saved and lastMessage updated successfully!:", conversationLastMessage);
 
     return { conversationLastMessage, persistedMessageID: message?._id };
   } catch (err) {
     logger.error("Error saving message or updating conversation:", err);
+
     throw err;
   }
 }
