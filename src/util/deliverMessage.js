4fc28b16c163602e54d15fdc2a341705535b112b
Aguthankgod <Aguthankgod@gmail.com>
2025-02-21 22:55:44 +0100

    feat: enhance message delivery with error handling and logging for offline users

--- src/util/deliverMessage.js
+++ src/util/deliverMessage.js
@@ -2,15 +2,23 @@
  * Delivers a message to the recipient if they are currently connected to the WebSocket server.
  *
  * @param {string} recipientID - The ID of the recipient to whom the message should be delivered.
- * @param {object} data - The message data to be delivered.
+ * @param {object} message - The message data to be delivered.
  */
-const deliverMessage = (recipientID, data, ActiveConnections) => {
-  if (ActiveConnections.has(recipientID)) {
-    ActiveConnections.get(recipientID).send(JSON.stringify(data));
-    return true;
-  } else {
-    return null;
+function deliverMessage(recipientID, message, ActiveConnections) {
+  if (!ActiveConnections.has(recipientID)) {
+    console.log(`⚠️ User ${recipientID} is not online. Cannot deliver message.`);
+    return false;
   }
-};
+
+  try {
+    const socket = ActiveConnections.get(recipientID);
+    console.log(`📨 Sending message to ${recipientID}:`, message);
+    socket.send(JSON.stringify(message));
+    return true;
+  } catch (error) {
+    console.log(`❌ Error delivering message to ${recipientID}:`, error);
+    return false;
+  }
+}
 
 module.exports = deliverMessage;
